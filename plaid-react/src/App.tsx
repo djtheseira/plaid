5689093302919fb96817b2be020b1b2a0c8aa558
Donovan Theseira <donovan.theseira@axientcorp.com>
2023-08-24 14:03:05 -0700

    feat: Fix when components are rendered to ensure no webpack issues, properly setup toast using container

--- plaid-react/src/App.tsx
+++ plaid-react/src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Route, Switch, withRouter } from "react-router-dom";
-import { toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.min.css';
 
-// import {} from "./components";
+import { UserPage, Landing, OAuthLink, Sockets } from "./components";
+
 import { AccountsProvider } from "./services/accounts";
 import { InstitutionsProvider } from "./services/institutions";
 import { ItemsProvider } from "./services/items";
@@ -14,19 +16,14 @@ import { ErrorsProvider } from "./services/errors";
 
 
 import './App.css';
-import { Landing, OAuthLink, UserPage } from "./components";
 
 function App() {
-    toast("toast-name", {
-        autoClose: 8000,
-        draggable: false,
-        className: 'box toast__background',
-        bodyClassName: 'toast__body',
-        hideProgressBar: true,
-    });
-
     return (
         <div className="App">
+            <ToastContainer
+                autoClose={8000}
+                draggable={false}
+            />
             <InstitutionsProvider>
                 <ItemsProvider>
                     <LinkProvider>
@@ -35,6 +32,7 @@ function App() {
                                 <ErrorsProvider>
                                     <UsersProvider>
                                         <CurrentUserProvider>
+                                            <Sockets/>
                                             <Switch>
                                                 <Route exact path="/" component={Landing} />
                                                 <Route path="/user/:userId" component={UserPage} />
