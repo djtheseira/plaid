695222a79af04dd955843a4dbcd9329e65d21916
Donovan Theseira <donovan.theseira@axientcorp.com>
2023-08-24 14:04:25 -0700

    chore: update import and exports of components

--- plaid-react/src/components/index.ts
+++ plaid-react/src/components/index.ts
@@ -1,11 +1,13 @@
 export { default as AccountCard } from './AccountCard';
 export { default as AddUserForm } from './AddUserForm';
 export { default as Banner } from './Banner';
+export { default as DuplicateItemToastMessage} from "./DuplicateItemToastMessage"
 export { default as Landing } from './Landing';
 export { default as LaunchLink } from './LaunchLink';
 export { default as ItemCard } from './ItemCard';
 export { default as OAuthLink } from './OAuthLink';
 export { default as SpendingInsights} from './SpendingInsights';
+export { default as Sockets} from './Sockets';
 export { default as UserCard } from './UserCard';
 export { default as UserDetails } from './UserDetails';
 export { default as UserList } from './UserList';
