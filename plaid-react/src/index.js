204b4ff0b011b42897d4ee87dc446ca2d53c11a6
Donovan Theseira <donovan.theseira@axientcorp.com>
2023-08-28 11:30:37 -0700

    chore: Update react router changes

--- plaid-react/src/index.js
+++ plaid-react/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {Router as BrowserRouter} from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import "./custom.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const history = createBrowserHistory();
 const root = document.getElementById("root");
 
 render(
-    <BrowserRouter history={history}>
+    <BrowserRouter >
         <App />
     </BrowserRouter>,
     root
