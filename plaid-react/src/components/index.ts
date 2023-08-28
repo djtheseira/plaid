4c4338f83806113960710541bf50b08bd778584c
Donovan Theseira <donovan.theseira@axientcorp.com>
2023-08-28 11:29:00 -0700

    chore: Add exports

--- plaid-react/src/components/index.ts
+++ plaid-react/src/components/index.ts
@@ -1,7 +1,12 @@
 export { default as AccountCard } from './AccountCard';
 export { default as AddUserForm } from './AddUserForm';
 export { default as Banner } from './Banner';
+export { default as Budget } from './Budget';
+export { default as BudgetPage } from './BudgetPage';
+export { default as BudgetCategoryCard } from './BudgetCategoryCard';
+export { default as BudgetCategoryTypeCategorySum } from './BudgetCategoryTypeCategorySum';
 export { default as DuplicateItemToastMessage} from "./DuplicateItemToastMessage"
+export { default as InfoPage } from './InfoPage';
 export { default as Landing } from './Landing';
 export { default as LaunchLink } from './LaunchLink';
 export { default as ItemCard } from './ItemCard';
